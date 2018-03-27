9fd569cd47264a357c5f92fb8201df8efb0072b6
crazyOrangeLee <616061897@qq.com>
2018-03-27 16:47:26 +0800

    add reminder dir 1st

---
+++ reminder/reminder.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @name crazyOrangeLee
+ * @desc chrome插件对应的api PHP处理文件
+ * @author Reagan Lee (616061897@qq.com)
+ */
+
+class test {
+	/**
+     * 测试函数
+     *
+     * @param null
+     * @return null
+     */
+	public function t()
+	{
+		echo "test";
+	}
+}
+
+function main()
+{
+	var_dump($_GET);
+
+}
+
+main()
+
+
+
+?>
